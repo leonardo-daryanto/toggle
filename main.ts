e32add329de859a139c73ebdbbcf30ff394f69ce
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-08-18 17:24:00 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
-})
+namespace basic {
+    /**
+     *  for changing string
+     */
+    //% block
+    //% group="Change"
+    export function change(stringGoingToChanged: string, changed: string): void {
+
+        basic.showString(stringGoingToChanged)
+        basic.pause(1000)
+
+        basic.showString(changed)
+    }
+}
